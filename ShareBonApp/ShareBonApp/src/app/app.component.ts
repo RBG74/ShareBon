55e81d2862ee3b359e4b7e0610ac30bb99da656c
ValentinB <valentin.bredy@hotmail.fr>
2017-12-06 12:08:19 +0100

    Statusbar round 4

--- ShareBonApp/ShareBonApp/src/app/app.component.ts
+++ ShareBonApp/ShareBonApp/src/app/app.component.ts
@@ -32,10 +32,7 @@ export class MyApp {
           Splashscreen.hide();
       });
       // let status bar overlay webview
-      this.statusBar.overlaysWebView(true);
-
-      // set status bar to white
-      this.statusBar.backgroundColorByHexString('#ffffff');
+      this.statusBar.hide;
   }
 
   openPage(page) {
